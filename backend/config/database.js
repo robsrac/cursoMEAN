50f7a8052e2fb18110ad227ec8ca9af4e7d9d74b
robsrac <robs.rac@gmail.com>
2017-08-17 17:05:32 -0300

    Seção 7 - Backend - Ciclo de Pagamento API
    48, 49, 50, 51, 52

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,2 +1,4 @@
 const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/db_finance', {useMongoClient: true})
+
+mongoose.Error.messages.general.required = "O abrituto '{PATH}' é obrigatório"
