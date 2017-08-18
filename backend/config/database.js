a81a07fadc889a93c014c00a45ea21ec0fc1dbf6
robsrac <robs.rac@gmail.com>
2017-08-18 17:06:14 -0300

    Seção 8 - Backend - Sumário de Pagamento API
    56

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,3 +2,6 @@ const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/db_finance', {useMongoClient: true})
 
 mongoose.Error.messages.general.required = "O abrituto '{PATH}' é obrigatório"
+mongoose.Error.messages.Number.min = "O valor '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O valor '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' não é valido para o atributo '{PATH}'"
